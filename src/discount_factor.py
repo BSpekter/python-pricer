0c76ab6b310380aec0ea674fdff469809e853a04
Nikita <61729611+KotikNikita@users.noreply.github.com>
2021-11-15 10:10:18 +0300

    Update discount_factor.py

--- src/discount_factor.py
+++ src/discount_factor.py
@@ -6,53 +6,55 @@ import sys
 sys.path.append('../')
 import datetime
 
-class discount_factors_curve(object):
-    def __init__(self):
-        
-#     @staticmethod
-    def discount_factors_array(self, dates):
-        data = pd.DataFrame(index = dates)
-        data['dates'] = np.nan
-        last_erodollar = 100 #Костылецкий
-        USD_rates = pd.read_csv('../data/USD rates.csv')
-        now = datetime.date.today()
-        USD_rates.index = USD_rates.EndDate
-        USD_rates = USD_rates['Unnamed: 1']
-        USD_rates[USD_rates.index[0]] = last_erodollar
-        tmp = np.exp(np.log(USD_rates).diff())
-        rates = tmp[tmp.index[2]:tmp.index[-1]]
-        rates = 1 / rates
-        rates = np.exp(np.log(rates).expanding().sum())
-        rates.index = pd.to_datetime(rates.index)
-        data.index = pd.to_datetime(data.index)
-        for_plotting = pd.concat([rates, data],axis=0).drop(['dates'],axis=1).sort_index()
-        for_plotting = pd.DataFrame(for_plotting)
-
-        for_plotting.index = pd.to_datetime(for_plotting.index)
-
-        for_plotting['interpolation'] = for_plotting.interpolate(method='time')
-        return 1 / for_plotting['interpolation'][dates]
-    
-    
 def discount_factors_array(dates):
-        data = pd.DataFrame(index = dates)
-        data['dates'] = np.nan
-        last_erodollar = 100 #Костылецкий
-        USD_rates = pd.read_csv('../data/USD rates.csv')
-        now = datetime.date.today()
-        USD_rates.index = USD_rates.EndDate
-        USD_rates = USD_rates['Unnamed: 1']
-        USD_rates[USD_rates.index[0]] = last_erodollar
-        tmp = np.exp(np.log(USD_rates).diff())
-        rates = tmp[tmp.index[2]:tmp.index[-1]]
-        rates = 1 / rates
-        rates = np.exp(np.log(rates).expanding().sum())
-        rates.index = pd.to_datetime(rates.index)
-        data.index = pd.to_datetime(data.index)
-        for_plotting = pd.concat([rates, data],axis=0).drop(['dates'],axis=1).sort_index()
-        for_plotting = pd.DataFrame(for_plotting)
+    data = pd.DataFrame(index = dates)
+    data['dates'] = np.nan
+    last_erodollar = 100 #Костылецкий
+    USD_rates = pd.read_csv('../data/USD rates.csv')
+    now = datetime.date.today()
+    USD_rates.index = USD_rates.EndDate
+    USD_rates = USD_rates['Unnamed: 1']
+    USD_rates[USD_rates.index[0]] = last_erodollar
+    tmp = np.exp(np.log(USD_rates).diff())
+    rates = tmp[tmp.index[2]:tmp.index[-1]]
+    rates = 1 / rates
+    rates = np.exp(np.log(rates).expanding().sum())
+    rates.index = pd.to_datetime(rates.index)
+    data.index = pd.to_datetime(data.index)
+    for_plotting = pd.concat([rates, data],axis=0).drop(['dates'],axis=1).sort_index()
+    for_plotting = pd.DataFrame(for_plotting)
 
-        for_plotting.index = pd.to_datetime(for_plotting.index)
+    for_plotting.index = pd.to_datetime(for_plotting.index)
 
-        for_plotting['interpolation'] = for_plotting.interpolate(method='time')
-        return 1 / for_plotting['interpolation'][dates]
+    for_plotting['interpolation'] = for_plotting.interpolate(method='time')
+    return 1 / for_plotting['interpolation'][dates]
+
+
+# class discount_factors_curve(object):
+#     def __init__(self):
+        
+# #     @staticmethod
+#     def discount_factors_array(self, dates):
+#         data = pd.DataFrame(index = dates)
+#         data['dates'] = np.nan
+#         last_erodollar = 100 #Костылецкий
+#         USD_rates = pd.read_csv('../data/USD rates.csv')
+#         now = datetime.date.today()
+#         USD_rates.index = USD_rates.EndDate
+#         USD_rates = USD_rates['Unnamed: 1']
+#         USD_rates[USD_rates.index[0]] = last_erodollar
+#         tmp = np.exp(np.log(USD_rates).diff())
+#         rates = tmp[tmp.index[2]:tmp.index[-1]]
+#         rates = 1 / rates
+#         rates = np.exp(np.log(rates).expanding().sum())
+#         rates.index = pd.to_datetime(rates.index)
+#         data.index = pd.to_datetime(data.index)
+#         for_plotting = pd.concat([rates, data],axis=0).drop(['dates'],axis=1).sort_index()
+#         for_plotting = pd.DataFrame(for_plotting)
+
+#         for_plotting.index = pd.to_datetime(for_plotting.index)
+
+#         for_plotting['interpolation'] = for_plotting.interpolate(method='time')
+#         return 1 / for_plotting['interpolation'][dates]
+    
+    
